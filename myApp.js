e61a093c3a6914a84cf62c49c25f193d00f13b64
gitcat456 <okothdenzel65@gmail.com>
2025-08-27 21:51:06 +0300

    app.METHOD(route,handler)

--- myApp.js
+++ myApp.js
@@ -1,7 +1,13 @@
 let express = require('express');
 let app = express();
 
+app.get("/", (req, res) => {
+    res.send("This is the HomePage fam");
+})
 
+app.get("/about", (req, res) => {
+    res.send("The server is serving gaddamit "); 
+})
 
 
 
